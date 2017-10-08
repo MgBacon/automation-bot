909c9bf094ae53b1e9e88d1f0eb5a01c38004d3b
Thomcdrom <Thom@icreate.nl>
2017-10-07 22:26:41 -0700

    Added react event and adds reactions to notewar anouncements

---
+++ app.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+var idAnouncment = '';
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.username}!`);
+    client.user.setGame("Life is a pain!");
+});
+
+client.on('message', msg => {
+
+    if (msg.channel.id === idAnouncment) {
+
+        if( msg.content.indexOf('Nodewar') && /((0[1-9]|1[0-2]).(0[1-9]|[1-2][0-9]|3[0-1]))|((0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]))((0[1/9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]))/.test(msg.content) && msg.content.indexOf('Please sign up') && client.user.id !== msg.author.id) {
+
+            msg.react('✅');
+            msg.react('❓');
+            msg.react('❎');
+
+            console.log('a new notewar anouncement got detected, emojis added.');
+        }
+    }
+    else {
+        if (msg.content === '.setAnouncement') {
+            msg.reply("This channel is now the anouncement channel!");
+            idAnouncment = msg.channel.id;
+        }
+    }
+});
+
+client.on('messageReactionAdd',  (reaction, user) => {
+
+    if(user.lastMessage.channel.id === idAnouncment && client.user.id !== user.id){
+
+        reaction.message.channel.send(user.username+" just reacted: "+reaction.emoji.name);
+}
+
+});
+client.login(process.env.DISCORD_TOKEN);
+
+module.exports=client;
